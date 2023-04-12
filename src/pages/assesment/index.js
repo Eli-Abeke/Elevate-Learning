5410b242a6e72c14ff67fd4db5a890ee27873485
Elisha <elisha.abeke@gmail.com>
2023-04-12 19:25:40 +0100

    Changes to be committed: 	modified:   src/pages/assesment/index.js

--- src/pages/assesment/index.js
+++ src/pages/assesment/index.js
@@ -28,7 +28,8 @@ export default function index() {
 
 
     const [isLoading, setLoading] = useState(true);
-  
+  const supabaseUrl = "http://localhost:54321/functions/v1/assesment_management"
+  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
     // Example POST method implementation  to trigger function
   async function postData() {
     if (typeof window !== 'undefined') {
